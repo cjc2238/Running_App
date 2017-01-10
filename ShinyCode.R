7ad91f9c119d4837f00c1c46a06d1c96879f0d56
Chad Coleman <cjc2238@tc.columbia.edu>
2017-01-10 11:41:19 -0500

    Basic Visuals of Running Data

--- ShinyCode.R
+++ ShinyCode.R
@@ -1,6 +1,7 @@
-## Pull Data from google Sheet
+library(shiny)
+library(googlesheets)
+library(RCurl)
 
-require(RCurl)
 fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
 fileCSV <- getURL(fileUrl,.opts=list(ssl.verifypeer=FALSE))
 fileCSVDF <-  read.csv(textConnection(fileCSV))
@@ -14,8 +15,30 @@ colnames(df1) <- c("person","miles")
 require(ggplot2)
 p <- ggplot(df, aes(time_stamp, miles, colour=person)) + geom_point(size=2) + geom_line(aes(colour=person, group=person), size=1) + labs(list(title = "Time Series of Running Data", x = "Date", y = "Total Miles Ran"))
 time_series <- p + scale_y_continuous(expand = c(0, .5))
-print(time_series)
 
 
 totals <- ggplot(data = df1, aes(person, miles, fill=person)) + geom_bar(stat = "identity") + geom_text(aes(label=df1$miles), vjust=2, size=4) + labs(list(title = "Total Miles Ran Per Person in 2017", x = "Person", y = "Total Distance Ran (miles)"))
-print(totals)
+
+
+if (interactive()) {
+  
+  ui <- fluidPage(
+    sliderInput("obs", "Number of observations", 0, 1000, 500),
+    actionButton("goButton", "Go!"),
+    plotOutput("distPlot")
+  )
+  
+  server <- function(input, output) {
+    output$distPlot <- renderPlot({
+      # Take a dependency on input$goButton. This will run once initially,
+      # because the value changes from NULL to 0.
+      input$goButton
+      
+      # Use isolate() to avoid dependency on input$obs
+      dist <- ggplot(data = df1, aes(person, miles, fill=person)) + geom_bar(stat = "identity") + geom_text(aes(label=df1$miles), vjust=2, size=4) + labs(list(title = "Total Miles Ran Per Person in 2017", x = "Person", y = "Total Distance Ran (miles)"))
+    })
+  }
+  
+  shinyApp(ui, server)
+  
+}
