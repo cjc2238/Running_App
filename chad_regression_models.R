87b5d9bd6c2f2581629749dac6e98277715f17be
Chad Coleman <cjc2238@tc.columbia.edu>
2017-02-23 12:24:12 -0500

    Created total prediction LM

--- chad_regression_models.R
+++ chad_regression_models.R
@@ -18,7 +18,7 @@ df$run_time_decimal <-  sapply(strsplit(df$run_time,":"),
 df$week <- format(df$time_stamp, format='%W')
 df$week <- as.numeric(df$week)
 df$week[df$week == 0] <- 1
-df$yday <- yday(df$time_stamp)
+df$day <- yday(df$time_stamp)
 df$year <- year(df$time_stamp)
 df$week[df$week == '00'] <- '01'
 df$weekday <- weekdays(df$time_stamp)
@@ -32,34 +32,40 @@ df1 <- aggregate(df$miles, by=list(person=df$person), FUN=sum)
 colnames(df1) <- c("person","miles")
 
 # Sum of miles per person per day
-df2 <- aggregate(df$miles, by=list(person=df$person, day=df$yday), FUN=sum)
+df2 <- aggregate(df$miles, by=list(person=df$person, day=df$day), FUN=sum)
 colnames(df2) <- c("person","day","miles")
 
-df3 <- aggregate(df$miles, by=list(yday=df$yday), FUN=sum)
+df3 <- aggregate(df$miles, by=list(day=df$day), FUN=sum)
 colnames(df3) <- c("day","miles")
 
 df4 <- aggregate(df$miles, by=list(person=df$person, week=df$week), FUN=sum)
 colnames(df4) <- c("person", "week","miles")
 
 # average run time per person per day
-df5 <- aggregate(df$run_time_decimal, by=list(person=df$person, day=df$yday), FUN=sum)
+df5 <- aggregate(df$run_time_decimal, by=list(person=df$person, day=df$day), FUN=sum)
 colnames(df5) <- c("person","day","run_time_decimal")
 
 # join aggregated daily run time and miles sum
 
 df6 <- merge(df2, df5, by=c("person","day"))
+df7 <- merge(df6, df, by.x='day', by.y='day')
+
+# Create person specific df
+
+df_chad <- df7[ which(df7$person.y=="Chad"), ]
+df_chad <- df_chad[ which(df_chad$person.x=="Chad"), ]
+df_chad <- df_chad[,-c(5:10)]
+df_chad <- unique(df_chad)
 
 # create time per mile 
 
-df6$mile_time <- df6$miles/df6$run_time_decimal
+df_chad$mile_time <- df_chad$miles/df_chad$run_time_decimal
+colnames(df_chad) <- c("day", "person","miles","run_time_decimal","week","year","weekday","mile_time")
 
-# Create person specific df
-df_chad <- df6[ which(df6$person=="Chad"), ]
+# drop person and year column
 
+df_chad <- df_chad[,-c(2,6)]
 
-# drop person column
-
-df_chad <- df_chad[,-1]
 
 # remove outliers
 
@@ -97,24 +103,25 @@ outlierKD <- function(dt, var) {
 
 df_chad1 <- outlierKD(df_chad, miles)
 no
-df_chad1 <- outlierKD(df_chad, run_time_decimal)
-no
+
 # remove rows with na values
 
 final_chad <- df_chad[complete.cases(df_chad),]
+final_chad$weekday <- as.factor(final_chad$weekday)
+final_chad$week <- as.numeric(final_chad$week)
+
+# Select only cases where I was jogging IE mile time >= 5.0 mph
+
+df_chad_jogging <- final_chad[ which(final_chad$mile_time>=5.0), ]
 
 # Fit lm Model
 
-model1 <- lm(mile_time ~ day, final_chad)
+model1 <- lm(mile_time ~ day + miles, df_chad_jogging)
 
 # get summary of the model
 
 summary(model1)
 
-# calculate correlation between day of year and miles ran
-attach(final_chad)
-cor(day, miles, method = "pearson")
-detach(final_chad)
 # create confidence interval
 
 confint(model1, conf.level=0.95)
@@ -140,4 +147,70 @@ ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) 
 
 # plot model
 p <- ggplotRegression(model1)
-p + theme_light()
+day_reg <- p + theme_light()
+
+# create regression for week
+
+# drop uneeded columns
+
+chad_week <- df_chad_jogging[,-c(1,5,6)]
+
+# aggregate data by week 
+
+chad_week1 <- aggregate(chad_week, by=list(chad_week$week), FUN=sum)
+
+# drop 4th columns
+
+chad_week2 <- chad_week1[,-4]
+
+# calulate mile time
+chad_week2$mile_time <- chad_week2$miles/chad_week2$run_time_decimal
+
+# rename columns
+
+colnames(chad_week2) <- c("week","miles","run_time_decimal","mile_time")
+
+# Fit week lm Model
+
+model2 <- lm(mile_time ~ week + miles, chad_week2)
+
+# get summary of the model
+
+summary(model2)
+
+# create confidence interval
+
+confint(model2, conf.level=0.95)
+
+# check the regression diagnostic plots for this model
+
+plot(model2)
+
+# create regression plot function
+
+ggplotRegression <- function (fit) {
+  
+  require(ggplot2)
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5),
+                       " P =",signif(summary(fit)$coef[2,4], 5)), x = "Week of Year", y = "Time to Complete Run (MPH)")
+}
+
+
+# plot model
+p <- ggplotRegression(model2)
+week_reg <- p + theme_light()
+week_reg
+
+# predict March 12 5k time
+predict(model1, data.frame(day = 71, miles = 3.2))
+predict(model2, data.frame(week = 11, miles = 3.2))
+
+require(gridExtra)
+
+grid.arrange(day_reg, week_reg, ncol=1)
---
+++ predict_total_distance.R
@@ -0,0 +1,96 @@
+require(RCurl)
+require(dplyr)
+require(lubridate)
+
+fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
+fileCSV <- getURL(fileUrl,.opts=list(ssl.verifypeer=FALSE))
+fileCSVDF <-  read.csv(textConnection(fileCSV))
+df <- fileCSVDF[,c(1:5)]
+
+# create time values
+df$time_stamp <- as.POSIXct(df$time_stamp, format='%m/%d/%Y %H:%M:%OS')
+df$run_time <- as.POSIXct(df$run_time, format='%H:%M:%OS')
+df$run_time <- format(df$run_time, format='%H:%M:%OS')
+df$run_time_decimal <-  sapply(strsplit(df$run_time,":"),
+                               function(x) {
+                                 x <- as.numeric(x)
+                                 x[1]+x[2]/60
+                               }
+)
+df$week <- format(df$time_stamp, format='%W')
+df$week <- as.numeric(df$week)
+df$week[df$week == 0] <- 1
+df$day <- yday(df$time_stamp)
+df$year <- year(df$time_stamp)
+df$week[df$week == '00'] <- '01'
+df$weekday <- weekdays(df$time_stamp)
+df$location[df$location == "Hell (treadmill)"] <- 'Treadmill'
+
+# Sum of miles per person per day
+df1 <- aggregate(df$miles, by=list(day=df$day), FUN=sum)
+
+# create year columns 
+
+df1$year <- 2017
+
+colnames(df1) <- c("day","miles","year")
+
+# create cumulative sum column
+
+df2 <- df1 %>% group_by(year) %>% mutate(cumsum = cumsum(miles))
+
+# drop year columns 
+
+df3 <- df2[,-c(2:3)]
+
+# fit model
+
+model1 <- lm(cumsum ~ day, df3)
+
+# view model
+summary(model1)
+
+# create confidence interval
+
+confint(model1, conf.level=0.95)
+
+# plot model
+plot(model1)
+
+# create regression plot function
+
+ggplotRegression <- function (fit) {
+  
+  require(ggplot2)
+  
+  ggplot(fit$model, aes_string(x = names(fit$model)[2], y = names(fit$model)[1])) + 
+    geom_point() +
+    stat_smooth(method = "lm", col = "red") +
+    labs(title = paste("Adj R2 = ",signif(summary(fit)$adj.r.squared, 5),
+                       "Intercept =",signif(fit$coef[[1]],5 ),
+                       " Slope =",signif(fit$coef[[2]], 5)), x = "Day of Year", y = "Total Distance Completed in Miles")
+  }
+
+# plot model
+p <- ggplotRegression(model1)
+total_reg <- p + theme_light() + theme(plot.title = element_text(hjust = 0.5))
+total_reg
+
+# create data frame to predict 
+
+year_df <- data.frame(day = 53:365)
+
+year_df$cumsum <- predict(model1, year_df)
+
+# combine prediction to total dataframe
+
+df4 <- rbind(df3, year_df)
+
+predicted_total <- ggplot(year_df, aes(day, cumsum)) + geom_line() + geom_smooth(method = "lm", col = "blue") + labs(title = "Predicted Year End Distance", x = "Day of Year", y = "Total Distance Completed in Miles") + geom_hline(yintercept = 2017, colour = "red", size = .8, linetype = "dotted") + annotate("text", x = 70, y = 1875, label = "2017 Miles", size = 4, colour = "black")
+
+predicted_reg <- predicted_total + theme_light() + theme(plot.title = element_text(hjust = 0.5)) + annotate("text", x = 360, y = 1250, label = "1489.58
+Miles", size = 4, colour = "blue")
+
+require(gridExtra)
+
+grid.arrange(total_reg, predicted_reg, ncol=1)
