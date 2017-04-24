ce6a23bdd0579d95e90df7df4af65f84f96dbfec
Chad Coleman <cjc2238@tc.columbia.edu>
2017-04-24 15:20:33 -0400

    Corrected plot layout

--- Test/app.R
+++ Test/app.R
@@ -1,47 +1,113 @@
 library(shiny)
+library(googlesheets)
+library(RCurl)
+require(lubridate)
 library(ggplot2)
 library(gridExtra)
 
+
+#####################
+## Begin Shiny App ##
+#####################
 u <- shinyUI(fluidPage(
-  titlePanel("title panel"),
-  sidebarLayout(position = "left",
-                sidebarPanel("sidebar panel",
-                             checkboxInput("donum1", "Make #1 plot", value = T),
-                             checkboxInput("donum2", "Make #2 plot", value = F),
-                             checkboxInput("donum3", "Make #3 plot", value = F),
-                             sliderInput("wt1","Weight 1",min=1,max=10,value=1),
-                             sliderInput("wt2","Weight 2",min=1,max=10,value=1),
-                             sliderInput("wt3","Weight 3",min=1,max=10,value=1)
-                ),
-                mainPanel("main panel",
-                          column(6,plotOutput(outputId="plotgraph", width="500px",height="400px"))
-                ))))
+  
+  titlePanel("Hello Shiny!"),
+  
+  fluidRow(
+    
+    column(2,
+           wellPanel(
+             selectInput("plot","Select the Plot",c("Amy","Chad","Lauren","Naresh"), selected = "Amy", selectize = TRUE),
+             submitButton(text = "Submit"))       
+    ),
+    
+    column(12,
+           plotOutput("plotgraph")
+    )
+  )
+))
+
+
+############
+## Server ##
+############
+
 
 s <- shinyServer(function(input, output) 
 {
-  set.seed(123)
-  pt1 <- reactive({
-    if (!input$donum1) return(NULL)
-    qplot(rnorm(500),fill=I("red"),binwidth=0.2,main="plotgraph1")
-  })
-  pt2 <- reactive({
-    if (!input$donum2) return(NULL)
-    qplot(rnorm(500),fill=I("blue"),binwidth=0.2,main="plotgraph2")
-  })
-  pt3 <- reactive({
-    if (!input$donum3) return(NULL)
-    qplot(rnorm(500),fill=I("green"),binwidth=0.2,main="plotgraph3")
-  })
-  output$plotgraph = renderPlot({
-    ptlist <- list(pt1(),pt2(),pt3())
-    wtlist <- c(input$wt1,input$wt2,input$wt3)
-    # remove the null plots from ptlist and wtlist
-    to_delete <- !sapply(ptlist,is.null)
-    ptlist <- ptlist[to_delete] 
-    wtlist <- wtlist[to_delete]
-    if (length(ptlist)==0) return(NULL)
-    
-    grid.arrange(grobs=ptlist,widths=wtlist,ncol=length(ptlist))
-  })
-})
+  output$plotgraph = renderPlot(grid.arrange(
+    {
+      pt1 <- ggplot(data = {fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
+      fileCSV <- getURL(fileUrl,.opts=list(ssl.verifypeer=FALSE))
+      fileCSVDF <-  read.csv(textConnection(fileCSV))
+      df <- fileCSVDF[,c(1:5)]
+      x <- as.POSIXct(df$time_stamp, format='%m/%d/%Y %H:%M:%OS')
+      df$run_time <- as.POSIXct(df$run_time, format='%H:%M:%OS')
+      df$run_time <- format(df$run_time, format='%H:%M:%OS')
+      df$week <- format(x, format='%W')
+      df$yday <- yday(x)
+      df$week[df$week == '00'] <- '01'
+      df$location[df$location == "Hell (treadmill)"] <- 'Treadmill'
+      df$person <- as.character(df$person)
+      df$week <- as.numeric(df$week)
+      df1 = subset(df, df$person %in% input$plot)
+      df2 <- aggregate(df1$miles, by=list(df1$week), 
+                       FUN=sum, na.rm=TRUE)
+      colnames(df2) <- c("week","miles")
+      df3 <<- as.data.frame(df2)},
+      aes(week, miles)) + geom_line(color="slategray3") + geom_point(size=.75, color="slategray4") +
+        geom_hline(yintercept = sum(df3$miles)/tail(df$week, n=1), color="indianred3", linetype = "dashed") +
+        labs(list( x = "Week of Year Beginning Jan 01 2017", y = "Miles Logged"))  +
+        annotate("text", x = 5, y = sum(df3$miles)/tail(df$week, n=1)+sum(df3$miles)/tail(df$week, n=1)*.05, label = {csvFileName <- paste(round(sum(df3$miles)/tail(df$week, n=1),2)," Average",sep="")}, size = 5.5, colour = "indianred3") +
+        ggtitle({csvFileName <- paste("Weekly Distance Logged For ", input$plot, " \n Total Distance ", sum(df3$miles), sep="")}) + theme_minimal() + 
+        theme(plot.title = element_text(hjust = .5))
+    },
+    {pt3 <- ggplot(data = {fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
+    fileCSV <- getURL(fileUrl,.opts=list(ssl.verifypeer=FALSE))
+    fileCSVDF <-  read.csv(textConnection(fileCSV))
+    df <- fileCSVDF[,c(1:5)]
+    x <- as.POSIXct(df$time_stamp, format='%m/%d/%Y %H:%M:%OS')
+    df$run_time <- as.POSIXct(df$run_time, format='%H:%M:%OS')
+    df$run_time <- format(df$run_time, format='%H:%M:%OS')
+    df$week <- format(x, format='%W')
+    df$yday <- yday(x)
+    df$week[df$week == '00'] <- '01'
+    df$location[df$location == "Hell (treadmill)"] <- 'Treadmill'
+    df$person <- as.character(df$person)
+    df$week <- as.numeric(df$week)
+    df2 <- aggregate(df$miles, by=list(df$person), 
+                     FUN=sum, na.rm=TRUE)
+    colnames(df2) <- c("person","miles")
+    df3 <<- as.data.frame(df2)},aes(person, miles, fill=person, label=miles)) + geom_bar(stat = "identity") + labs(list( x = "Person", y = "Miles Logged", fill = "")) + scale_fill_brewer(palette="Set3") + ggtitle(paste("Miles Logged Per Person \n Total Miles completed = ", sum(df$miles), sep = "")) + theme_minimal() + geom_text(aes(label=miles), position=position_dodge(width=0.9), vjust=-0.25)
+    pt3 + theme(legend.position = "NA") + theme(plot.title = element_text(hjust = .5)) 
+    },
+    {
+      pt2 <- ggplot(data = {fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
+      fileCSV <- getURL(fileUrl,.opts=list(ssl.verifypeer=FALSE))
+      fileCSVDF <-  read.csv(textConnection(fileCSV))
+      df <- fileCSVDF[,c(1:5)]
+      x <- as.POSIXct(df$time_stamp, format='%m/%d/%Y %H:%M:%OS')
+      df$run_time <- as.POSIXct(df$run_time, format='%H:%M:%OS')
+      df$run_time <- format(df$run_time, format='%H:%M:%OS')
+      df$week <- format(x, format='%W')
+      df$yday <- yday(x)
+      df$week[df$week == '00'] <- '01'
+      df$location[df$location == "Hell (treadmill)"] <- 'Treadmill'
+      df$person <- as.character(df$person)
+      df$week <- as.numeric(df$week)
+      d <- as.data.frame(aggregate(df$miles, by=list(df$location, df$person), 
+                                   FUN=sum, na.rm=TRUE))
+      colnames(d) <- c("Location","person","miles")
+      dd <- as.data.frame(d)
+      },aes(x = person, y = miles, fill = Location, label = miles)) +
+        geom_bar(stat = "identity") +
+        labs(list( x = "Person", y = "Miles Logged", fill = "")) +
+        geom_text(size = 2.5, position = position_stack(vjust = 0.5)) + ggtitle("Where Miles Were Logged") + theme_minimal()
+      pt2 + theme(legend.position = "top") + coord_polar("x") + scale_fill_brewer(palette="Set3") + theme(plot.title = element_text(hjust = .5))
+    },ncol=3)
+  )})
+
+#########
+## APP ##
+#########
 shinyApp(u,s)
--- app.R
+++ app.R
@@ -15,7 +15,7 @@ u <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("plot","Select the Plot",c("Amy","Chad","Lauren","Naresh"), selected = "Amy",                  selectize = TRUE),
-      submitButton(text = "Submit"), width = 2
+      submitButton(text = "Submit"), width = 1
     ),
     mainPanel("",
               column(12,plotOutput(outputId="plotgraph"))
@@ -44,14 +44,34 @@ s <- shinyServer(function(input, output)
                      FUN=sum, na.rm=TRUE)
     colnames(df2) <- c("week","miles")
     df3 <<- as.data.frame(df2)},
-    aes(week, miles)) + geom_line() + geom_point() +
-        geom_hline(yintercept = sum(df3$miles)/tail(df$week, n=1), color="skyblue3", linetype = "dashed") +
+    aes(week, miles)) + geom_line(color="slategray3") + geom_point(size=.75, color="slategray4") +
+        geom_hline(yintercept = sum(df3$miles)/tail(df$week, n=1), color="indianred3", linetype = "dashed") +
         labs(list( x = "Week of Year Beginning Jan 01 2017", y = "Miles Logged"))  +
-        annotate("text", x = 1.5, y = sum(df3$miles)/tail(df$week, n=1)+sum(df3$miles)/tail(df$week, n=1)*.05, label = {csvFileName <- paste(round(sum(df3$miles)/tail(df$week, n=1),2)," Average",sep="")}, size = 4, colour = "indianred3") +
-        ggtitle({csvFileName <- paste("Weekly Distance Logged For ", input$plot, " \n Total Distance ", sum(df3$miles), sep="")}) + theme_linedraw() + 
+        annotate("text", x = 5, y = sum(df3$miles)/tail(df$week, n=1)+sum(df3$miles)/tail(df$week, n=1)*.05, label = {csvFileName <- paste(round(sum(df3$miles)/tail(df$week, n=1),2)," Average",sep="")}, size = 5.5, colour = "indianred3") +
+        ggtitle({csvFileName <- paste("Weekly Distance Logged For ", input$plot, " \n Total Distance ", sum(df3$miles), sep="")}) + theme_minimal() + 
         theme(plot.title = element_text(hjust = .5))
-    },{
-  pt2 <- ggplot(data = {fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
+    },
+  {pt3 <- ggplot(data = {fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
+  fileCSV <- getURL(fileUrl,.opts=list(ssl.verifypeer=FALSE))
+  fileCSVDF <-  read.csv(textConnection(fileCSV))
+  df <- fileCSVDF[,c(1:5)]
+  x <- as.POSIXct(df$time_stamp, format='%m/%d/%Y %H:%M:%OS')
+  df$run_time <- as.POSIXct(df$run_time, format='%H:%M:%OS')
+  df$run_time <- format(df$run_time, format='%H:%M:%OS')
+  df$week <- format(x, format='%W')
+  df$yday <- yday(x)
+  df$week[df$week == '00'] <- '01'
+  df$location[df$location == "Hell (treadmill)"] <- 'Treadmill'
+  df$person <- as.character(df$person)
+  df$week <- as.numeric(df$week)
+  df2 <- aggregate(df$miles, by=list(df$person), 
+                   FUN=sum, na.rm=TRUE)
+  colnames(df2) <- c("person","miles")
+  df3 <<- as.data.frame(df2)},aes(person, miles, fill=person, label=miles)) + geom_bar(stat = "identity") + labs(list( x = "Person", y = "Miles Logged", fill = "")) + scale_fill_brewer(palette="Set3") + ggtitle(paste("Miles Logged Per Person \n Total Miles completed = ", sum(df$miles), sep = "")) + theme_minimal() + geom_text(aes(label=miles), position=position_dodge(width=0.9), vjust=-0.25)
+  pt3 + theme(legend.position = "NA") + theme(plot.title = element_text(hjust = .5)) 
+  },
+  {
+    pt2 <- ggplot(data = {fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
     fileCSV <- getURL(fileUrl,.opts=list(ssl.verifypeer=FALSE))
     fileCSVDF <-  read.csv(textConnection(fileCSV))
     df <- fileCSVDF[,c(1:5)]
@@ -70,25 +90,9 @@ s <- shinyServer(function(input, output)
     dd <- as.data.frame(d)
     },aes(x = person, y = miles, fill = Location, label = miles)) +
       geom_bar(stat = "identity") +
-            labs(list( x = "Person", y = "Miles Logged", fill = "")) +
-      geom_text(size = 2.5, position = position_stack(vjust = 0.5)) + 
-      theme_linedraw()
-  pt2 + theme(legend.position = "top") + coord_polar("x") + scale_fill_brewer(palette="Set3")
-  },
-  {ggplot(data = {fileUrl <- "https://docs.google.com/spreadsheets/d/1RTRABrYQhyyfj_Dr3bkmNSnxmM7jbXIMgrI8eOa7Avk/export?format=csv"
-  fileCSV <- getURL(fileUrl,.opts=list(ssl.verifypeer=FALSE))
-  fileCSVDF <-  read.csv(textConnection(fileCSV))
-  df <- fileCSVDF[,c(1:5)]
-  x <- as.POSIXct(df$time_stamp, format='%m/%d/%Y %H:%M:%OS')
-  df$run_time <- as.POSIXct(df$run_time, format='%H:%M:%OS')
-  df$run_time <- format(df$run_time, format='%H:%M:%OS')
-  df$week <- format(x, format='%W')
-  df$yday <- yday(x)
-  df$week[df$week == '00'] <- '01'
-  df$location[df$location == "Hell (treadmill)"] <- 'Treadmill'
-  df$person <- as.character(df$person)
-  df$week <- as.numeric(df$week)
-  df1 <- as.data.frame(df)},aes(person, miles, fill=person, label=miles)) + geom_bar(stat = "identity") + labs(list( x = "Person", y = "Miles Logged", fill = "")) + scale_fill_brewer(palette="Set3")
+      labs(list( x = "Person", y = "Miles Logged", fill = "")) +
+      geom_text(size = 2.5, position = position_stack(vjust = 0.5)) + ggtitle("Where Miles Were Logged") + theme_minimal()
+    pt2 + theme(legend.position = "top") + coord_polar("x") + scale_fill_brewer(palette="Set3") + theme(plot.title = element_text(hjust = .5))
   },ncol=3)
   )})
 
